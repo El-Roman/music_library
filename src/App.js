03423e185910f886e73efd2a6f4677021f79f0b4
Roman M <rmarchen95@gmail.com>
2023-03-16 21:46:04 -0700

    react-router-dom
    expanding apps with react-router-dom, worked out some bugs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
-import { DataContext } from './context/DataContext'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
 
 function App() {
     let [search, setSearch] = useState('')
@@ -17,9 +19,9 @@ function App() {
                 const response = await fetch(API_URL + search)
                 const resData = await response.json()
                 if (resData.results.length > 0) {
-                    setData(resData.results)
+                    return setData(resData.results)
                 } else {
-                    setMessage('Not Found')
+                    return setMessage('Not Found')
                 }
             }
             fetchData()
@@ -33,15 +35,24 @@ function App() {
 
     return (
         <div>
-            <SearchBar handleSearch={handleSearch} />
             {message}
-            <DataContext.Provider value={data}>
-                <Gallery />
-            </DataContext.Provider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={
+                        <>
+                            <SearchBar handleSearch={handleSearch} />
+                            <Gallery data={data} />
+                        </>
+                    } />
+                    <Route path="/album/:id" element={<AlbumView />} />
+                    <Route path="/artist/:id" element={<ArtistView />} />
+                </Routes>
+            </Router>
         </div>
     );
 }
 
 export default App;
+
 
 
